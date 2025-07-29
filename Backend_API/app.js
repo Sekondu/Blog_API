988f62e83770f43cef992cbc55ba537418c3473e
musashi <rwid2005@gmail.com>
2025-07-29 14:15:50 +0300

    final adjustments for backend

--- Backend_API/app.js
+++ Backend_API/app.js
@@ -10,9 +10,26 @@ const cors=require("cors");
 const multer=require("multer");
 const app=express();
 
+const allowedOrigins=[
+    "https://blog-api-1-90mf.onrender.com",
+]
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // Allow non-browser requests
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // if your frontend needs cookies/auth
+}));
+
 app.use("/Controllers",express.static(path.join(__dirname,'Controllers')));
 
 function authenticateToken(req,res,next){
